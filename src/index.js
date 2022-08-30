15a310555af19386aede180806f69c352c803f3b
Kyle Chinick <kylechinick@icloud.com>
2022-08-30 15:23:33 -0700

    Add support to display player character stats as they are changed
    Co-authored-by: Matt Herbert <Matth5050@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -8,28 +8,20 @@ let plant = { soil: 5, light: 0, water: 0, hp: 10 };
 // const feed = changeState('soil');
 // const hydrate = changeState('water');
 // const giveLight = changeState('light');
-
 // const greenFood = changeState('soil')(10);
 // const yuckyFood = changeState('soil')(-5);
-
 // console.log('plant - blueFood', blueFood(plant));
 
-const damagingDandelion = { ...plant, water: 12 };
-// does extra damage
+// const damagingDandelion = { ...plant, water: 12 };
 
-const scrappySunflower = { ...plant };
-// some unique move
+// const scrappySunflower = { ...plant };
 
-const radRose = { ...plant };
-// lives in your grandparents' yard
+// const radRose = { ...plant };
 
-console.log('damagingDandelion', damagingDandelion);
+// console.log('damagingDandelion', damagingDandelion);
 
-// const dandelionPunch = changeState('hp')(-5);
-// console.log('scrappySunflower', dandelionPunch(scrappySunflower));
-
-const storeState = () => {
-  let currentState = {};
+const storeState = initialValue => {
+  let currentState = initialValue;
   return (stateChangeFunction = state => state) => {
     const newState = stateChangeFunction(currentState);
     currentState = { ...newState };
@@ -38,6 +30,8 @@ const storeState = () => {
 };
 
 const stateControl = storeState();
+const stateControl2 = storeState();
+const stateControl3 = storeState();
 
 const changeState = prop => {
   return value => {
@@ -49,27 +43,56 @@ const changeState = prop => {
 };
 
 const blueFood = changeState('soil')(5);
+const greenFood = changeState('soil')(10);
 const dandelionPunch = changeState('hp')(-5);
-
-const fedPlant = stateControl(blueFood);
-const currentState = stateControl();
-console.log(currentState.soil);
-console.log(currentState);
 
 // UI Stuff
 
 $(document).ready(function () {
+  let plants = [];
   $('#player-choice-form').submit(function (event) {
     event.preventDefault();
-    let playerChoice = $("input[type=radio][name=survey]:checked").val();
+    let playerChoice = $('input[type=radio][name=survey]:checked').val();
     console.log(playerChoice);
     $('#fighter-choice').text(playerChoice);
+
+    const playerGenerator = () => {
+      const plant1 = storeState(plant);
+      // const plant2 = stateControl(blueFood);
+      plants.push(plant1);
+      console.log(plant1());
+      $('#player-stats-soil').text(plant1().soil);
+      $('#player-stats-light').text(plant1().light);
+      $('#player-stats-water').text(plant1().water);
+      $('#player-stats-hp').text(plant1().hp);
+    };
+    playerGenerator();
+  });
+
+  $('#player-actions').click(function () {
+    plants[0](blueFood);
+    console.log(plants[0]());
+    // $('#player-stats').text(plants[0]());
+    $('#player-stats-soil').text(plants[0]().soil);
+    $('#player-stats-light').text(plants[0]().light);
+    $('#player-stats-water').text(plants[0]().water);
+    $('#player-stats-hp').text(plants[0]().hp);
   });
 });
 
 // var value = $("input[type=radio][name=contact]:checked").val();
 
-// const plantFedAgain = stateControl(greenFood);
+// const plant1 = stateControl(blueFood);
+// const plant2 = stateControl(blueFood);
+// const plant3 = stateControl(blueFood);
+// const plant4 = stateControl2(greenFood);
+// const plant5 = stateControl3(greenFood);
 
-// console.log('fedPlant', fedPlant);
-// console.log('plantFedAgain', plantFedAgain);
+// console.log('plant 1 ', plant1);
+// console.log('plant 2 ', plant2);
+// console.log('plant 3 ', plant3);
+// console.log('plant 4 ', plant4);
+// console.log('plant 5 ', plant5);
+
+// const stateControl = storeState();
+// const stateControl2 = storeState();
